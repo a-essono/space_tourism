7ef51fd1d8d718fa7ce001050bd0cda95e72746f
em-abessolo <em-abessolo@gmail.com>
2025-10-17 08:15:22 +0200

    création de la table planets

---
+++ database/migrations/2025_10_16_124939_create_planets_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('planets', function (Blueprint $table) {
+            $table->id();
+            $table->string('nom_fr', 50);
+            $table->string('description_fr', 250);
+            $table->smallInteger('distance_fr');
+            $table->tinyInteger('duree_fr');
+            $table->string('nom_en', 50);
+            $table->string('description_en', 250);
+            $table->smallInteger('distance_en');
+            $table->tinyInteger('duree_en');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('planets');
+    }
+};
