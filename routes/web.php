516906382777a4b7eeac7ede03abfd624cc83e7c
em-abessolo <em-abessolo@gmail.com>
2025-11-24 16:02:49 +0100

    refactorisation suppression de la logique de comptage des routes

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,9 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+/**
+ * Tableau de bord principal
+ **/
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
@@ -36,7 +39,7 @@ $languages = [
         'prefix' => 'travels',
         'names' => 'en.',
     ],
-    'fr' =>[
+    'fr' => [
         'prefix' => 'voyages',
         'names' => 'fr.',
     ],
@@ -48,9 +51,9 @@ foreach ($languages as $locale => $config) {
         ->name($config['names'])
         ->group(function () use ($config) {
             Route::prefix($config['prefix'])->group(function () {
-               Route::get('/index', function () {
+                Route::get('/index', function () {
                     return view('travels/index');
-                })->name('accueil'); 
+                })->name('accueil');
 
                 Route::get('/crew/{crew}', [CrewController::class, 'show'])
                     ->name('equipage');
@@ -67,76 +70,65 @@ foreach ($languages as $locale => $config) {
 /**
  * Route Breeze du fichier auth.php
  **/
-require __DIR__.'/auth.php';
-
-/**
- * Tableau de bord principal
- **/
-Route::get('/dashboard', function () {
-    return view('dashboard', [
-        'planetCount' => \App\Models\Planet::count(),
-        'crewCount' => \App\Models\Crew::count(),
-        'techCount' => \App\Models\Technology::count(),
-    ]);
-})->middleware(['auth', 'verified'])->name('dashboard');
+require __DIR__ . '/auth.php';
 
 /**
  * Route Admin
  **/
 Route::prefix('dashboard')
-->name('admin.')
-->middleware(['auth', 'verified'])
-->group(function () {
-/**
- * Route Admin HOME
- **/
-// Page d'accueil admin pour la gestion des listes
-Route::get('/home', [\App\Http\Controllers\Admin\AdminHomeController::class, 'index'])->name('home');
+    ->name('admin.')
+    ->middleware(['auth', 'verified'])
+    ->group(function () {
+        /**
+         * Route Admin HOME
+         **/
+        // Page d'accueil admin pour la gestion des listes
+        Route::get('/home', [\App\Http\Controllers\Admin\AdminHomeController::class, 'index'])->name('home');
 
-/**
- * Route Admin PLANET
- **/
-Route::get('admin/planetes', [PlanetController::class, 'index'])->name('planetes.index');
-// Affiche le formulaire de création
-Route::get('admin/planetes/creation', [PlanetController::class, 'create'])->name('planetes.create');
-// Enregistre une nouvelle planète
-Route::post('admin/planetes/enregistrement', [PlanetController::class, 'store'])->name('planetes.store');
-// Affiche le formulaire de modification pour une planète spécifique
-Route::get('admin/planetes/{planet}/modification', [PlanetController::class, 'edit'])->where('planet', '[0-9]+')->name('planetes.edit');
-// Met à jour les données d'une planète existante
-Route::put('admin/planetes/{planet}', [PlanetController::class, 'update'])->where('planet', '[0-9]+')->name('planetes.update');
-// Supprime une planète existante
-Route::delete('admin/planetes/{planet}/suppression', [PlanetController::class, 'destroy'])->where('planet', '[0-9]+')->name('planetes.destroy');
+        /**
+         * Route Admin PLANET
+         **/
+        Route::get('admin/planetes', [PlanetController::class, 'index'])->name('planetes.index');
+        // Affiche le formulaire de création
+        Route::get('admin/planetes/creation', [PlanetController::class, 'create'])->name('planetes.create');
+        // Enregistre une nouvelle planète
+        Route::post('admin/planetes/enregistrement', [PlanetController::class, 'store'])->name('planetes.store');
+        // Affiche le formulaire de modification pour une planète spécifique
+        Route::get('admin/planetes/{planet}/modification', [PlanetController::class, 'edit'])->where('planet', '[0-9]+')->name('planetes.edit');
+        // Met à jour les données d'une planète existante
+        Route::put('admin/planetes/{planet}', [PlanetController::class, 'update'])->where('planet', '[0-9]+')->name('planetes.update');
+        // Supprime une planète existante
+        Route::delete('admin/planetes/{planet}/suppression', [PlanetController::class, 'destroy'])->where('planet', '[0-9]+')->name('planetes.destroy');
 
-/**
- * Route Admin CREW
- **/
-Route::get('admin/equipes', [CrewController::class, 'index'])->name('equipes.index');
-Route::get('admin/equipes/creation', [CrewController::class, 'create'])->name('equipes.create');
-Route::post('admin/equipes/enregistrement', [CrewController::class, 'store'])->name('equipes.store');
-Route::get('admin/equipes/{crew}/modification', [CrewController::class, 'edit'])->where('crew', '[0-9]+')->name('equipes.edit');
-Route::put('admin/equipes/{crew}', [CrewController::class, 'update'])->where('crew', '[0-9]+')->name('equipes.update');
-// Supprime une équipe existante
-Route::delete('admin/equipes/{crew}/suppression', [CrewController::class, 'destroy'])->where('crew', '[0-9]+')->name('equipes.destroy');
+        /**
+         * Route Admin CREW
+         **/
+        Route::get('admin/equipes', [CrewController::class, 'index'])->name('equipes.index');
+        Route::get('admin/equipes/creation', [CrewController::class, 'create'])->name('equipes.create');
+        Route::post('admin/equipes/enregistrement', [CrewController::class, 'store'])->name('equipes.store');
+        Route::get('admin/equipes/{crew}/modification', [CrewController::class, 'edit'])->where('crew', '[0-9]+')->name('equipes.edit');
+        Route::put('admin/equipes/{crew}', [CrewController::class, 'update'])->where('crew', '[0-9]+')->name('equipes.update');
+        // Supprime une équipe existante
+        Route::delete('admin/equipes/{crew}/suppression', [CrewController::class, 'destroy'])->where('crew', '[0-9]+')->name('equipes.destroy');
 
-/**
- * Route Admin TECH
- **/
-Route::get('admin/technologies', [TechnologyController::class, 'index'])->name('technologies.index');
-Route::get('admin/technologies/creation', [TechnologyController::class, 'create'])->name('technologies.create');
-Route::post('admin/technologies/enregistrement', [TechnologyController::class, 'store'])->name('technologies.store');
-Route::get('admin/technologies/{technology}/modification', [TechnologyController::class, 'edit'])->where('technology','[0-9]+')->name('technologies.edit');
-Route::put('admin/technologies/{technology}', [TechnologyController::class, 'update'])->where('technology', '[0-9]+')->name('technologies.update');
-// Supprime une technologie existante
-Route::delete('admin/technologies/{technology}', [TechnologyController::class, 'destroy'])->where('technology', '[0-9]+')->name('technologies.destroy');
-});
+        /**
+         * Route Admin TECH
+         **/
+        Route::get('admin/technologies', [TechnologyController::class, 'index'])->name('technologies.index');
+        Route::get('admin/technologies/creation', [TechnologyController::class, 'create'])->name('technologies.create');
+        Route::post('admin/technologies/enregistrement', [TechnologyController::class, 'store'])->name('technologies.store');
+        Route::get('admin/technologies/{technology}/modification', [TechnologyController::class, 'edit'])->where('technology', '[0-9]+')->name('technologies.edit');
+        Route::put('admin/technologies/{technology}', [TechnologyController::class, 'update'])->where('technology', '[0-9]+')->name('technologies.update');
+        // Supprime une technologie existante
+        Route::delete('admin/technologies/{technology}', [TechnologyController::class, 'destroy'])->where('technology', '[0-9]+')->name('technologies.destroy');
+    });
 
 /**
  * Route Admin avec un users.manage
  **/
 Route::prefix('admin')
     ->name('admin.')
-    ->middleware(['auth','verified','permission:crews.view']) // ou 'role:admin'
+    ->middleware(['auth', 'verified', 'permission:crews.view']) // ou 'role:admin'
     ->group(function () {
         // Gestion des rôles assignés aux utilisateurs
         Route::get('users', [UserRoleController::class, 'index'])->name('users.index');
