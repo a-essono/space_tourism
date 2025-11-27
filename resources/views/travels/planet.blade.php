655284c2116b1edc4212bda16a43cd14d5b97f59
em-abessolo <em-abessolo@gmail.com>
2025-11-27 08:47:46 +0100

    Image: ajout d'un blur pour une ombre sur planete (rectification des marges)

--- resources/views/travels/planet.blade.php
+++ resources/views/travels/planet.blade.php
@@ -3,20 +3,30 @@
         bg_planet
     </x-slot:bg>
 
-
     <main class="flex-1">
         <section class="text-center 
     flex-1 flex flex-col justify-center items-center lg:flex-row px-4">
-            <section class="w-full max-w-4xl">
+            <section class="relative w-full max-w-4xl">
                 <h2 class="text-[16px] leading-[19px] tracking-[2.7px] md:text-[20px] md:leading-[23px] md:tracking-[3.38px] 
             lg:text-[28px] lg:leading-[33px] lg:tracking-[4.72] text-[var(--white-25)] 
             uppercase font-barlow 
-            md:text-start mb-6 m-16">
+            md:text-start mb-6 mt-24 m-16">
                     <span class="opacity-25">01</span> {{ __('messages.h2_planet') }}
                 </h2>
-                <div class="mx-auto w-[170px] h-[170px] md:w-[300px] md:h-[300px] lg:w-[515px] lg:h-[527px] 
-            flex items-center justify-center">
-                    <img src="{{ $planet->image_url  }}" alt="{{ $planet->{'nom_' . app()->getLocale()} }}" class="w-full h-full object-contain">
+                <div class="relative mx-auto w-[170px] h-[170px] md:w-[300px] md:h-[300px] lg:w-[445px] lg:h-[445px] rounded-full overflow-hidden">
+                    <!-- Planètes -->
+                    <div class="absolute -left-[2.87px] -top-[2.87px] w-[175.73px] h-[175.73px]
+                md:-left-[5px] md:-top-[5px] md:w-[310px] md:h-[310px]
+                lg:-left-[7.5px] lg:-top-[7.5px] lg:w-[460px] lg:h-[460px] z-10">
+                        <img src="{{ $planet->image_url }}" alt="{{ $planet->{'nom_' . app()->getLocale()} }}"
+                            class="w-full h-full object-cover rounded-full">
+                    </div>
+                    <!-- Ombre floue -->
+                    <div class="absolute left-[15.28px] top-[26.74px] w-[290.72px] h-[290.72px]
+                md:left-[27px] md:top-[47px] md:w-[513px] md:h-[513px]
+                lg:left-[40px] lg:top-[70px] lg:w-[761px] lg:h-[761px]
+                bg-[#0b0d17] rounded-full filter blur-[25px] z-10 pointer-events-none">
+                    </div>
                 </div>
             </section>
             <section>
@@ -25,7 +35,7 @@
                 flex items-center justify-center m-5">
                         <ul class="text-[14px] leading-[16px] tracking-[2.36px] md:text-[16px] md:leading-[19px] md:tracking-[2.7px] 
                     text-[var(--white-25)] uppercase whitespace-nowrap 
-                    flex gap-6 ">
+                    flex gap-6 mt-20">
                             <x-list_planet :planets="$planets" />
                             <!-- @include('components.list_planet', ['planets' => $planets]) -->
                         </ul>
@@ -75,7 +85,6 @@
                 </section>
             </section>
         </section>
-        <?php echo $segment1 = app()->getLocale();  ?>
         <x-switch_lang />
     </main>
 </x-layout_header>
